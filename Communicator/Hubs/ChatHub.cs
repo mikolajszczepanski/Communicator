6d87b3510ca349a2a77c16c1bb437cb729227424
Mikołaj Szczepański <mikolaj.szczepanski@outlook.com>
2015-07-25 22:07:37 +0200

    Prosta implementacja czata.

---
+++ Communicator/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Communicator.Hubs
+{
+    public class ChatHub : Hub
+    {
+        /*public void Hello()
+        {
+            Clients.All.hello();
+        }*/
+
+        public void Send(string name, string message)
+        {
+            
+            string dateTime = DateTime.Now.Hour.ToString().PadLeft(2,'0') + 
+                        ":" + DateTime.Now.Minute.ToString().PadLeft(2, '0');
+
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message, dateTime);
+        }
+    }
+}
