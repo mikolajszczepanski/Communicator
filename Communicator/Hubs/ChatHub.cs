f923966e2b28cc219f4447223bcdb979f59390d5
Mikołaj Szczepański <mikolaj.szczepanski@outlook.com>
2015-07-26 20:06:09 +0200

    Zapis do bazy danych

--- Communicator/Hubs/ChatHub.cs
+++ Communicator/Hubs/ChatHub.cs
@@ -5,11 +5,15 @@ using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.Identity;
 using System.Web.Security;
+using Communicator.Models;
 
 namespace Communicator.Hubs
 {
     public class ChatHub : Hub
     {
+        private DefaultConnection db = new DefaultConnection();
+
+
         public void Send(string message,
                          string userIdTo,
                          string userIdFrom,
@@ -19,6 +23,17 @@ namespace Communicator.Hubs
                 // string name = Membership.GetUser(userIdFrom).UserName; //exception
                 string dateTime = DateTime.Now.Hour.ToString().PadLeft(2, '0') +
                         ":" + DateTime.Now.Minute.ToString().PadLeft(2, '0');
+
+                Message SendedMessage = new Message(message,
+                                                    userIdTo,
+                                                    userIdFrom,
+                                                    DateTime.Now,
+                                                    null,
+                                                    null,
+                                                    MessageFlagType.MESSAGE_WAITING_FOR_DELIVER);
+
+                db.Messages.Add(SendedMessage);
+                db.SaveChangesAsync();
 
                 Clients.All.broadcastMessage(userIdFrom, message, dateTime);
             }
---
+++ Communicator/Models/Message.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Communicator.Models
+{
+    public enum MessageFlagType
+    {
+        MESSAGE_NULL,
+        MESSAGE_WAITING_FOR_DELIVER,
+        MESSAGE_DELIVERED
+    }
+
+    public class Message
+    {
+
+        public Message()
+        {
+            Content = null;
+            UserIdTo = null;
+            UserIdFrom = null;
+            DateTimeSended = DateTime.Now;
+            IpAddress = null;
+            Token = null;
+            Flag = MessageFlagType.MESSAGE_NULL;
+
+        }
+        public Message(string _Content,
+                       string _UserIdTo,
+                       string _UserIdFrom,
+                       DateTime _DateTimeSended,
+                       string _IpAddress,
+                       string _Token,
+                       MessageFlagType _Flag)
+        {
+            Content = _Content;
+            UserIdTo = _UserIdTo;
+            UserIdFrom = _UserIdFrom;
+            DateTimeSended = _DateTimeSended;
+            IpAddress = _IpAddress;
+            Token = _Token;
+            Flag = _Flag;
+        }
+
+        public int Id { get; private set; }
+        public string Content { get; private set; }
+        public string UserIdTo { get; private set; }
+        public string UserIdFrom { get; private set; }
+        public DateTime DateTimeSended { get; private set; }
+        public string IpAddress { get; private set; }
+        public string Token { get; private set; }
+        public MessageFlagType Flag { get; set; }
+    }
+
+    public class DefaultConnection : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+    }
+}
